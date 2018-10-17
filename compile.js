fae6967720086231094582b8f9ab610dd7d64511
Yerden II, Gerald <Gerald.Yerden@duke-energy.com>
2018-10-17 08:12:15 -0400

    compile.js

---
+++ compile.js
@@ -0,0 +1,14 @@
+// We need the 'fs' and 'path' module because we cannot just require or import a '.sol' file.
+// NodeJS tries to execute as javascript anything that you 'import' or 'require'
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+
+
+// Getting the path to our Contract[s]
+const ethInboxPath = path.resolve(__dirname,'contracts','EthInbox.sol')
+// Reading our file line by line
+const sourceCode = fs.readFileSync(ethInboxPath, 'utf-8')
+
+// solc requires the source code we are reading in, as well as the number of contracts we are trying to compile
+solc.compile(sourceCode,1)
