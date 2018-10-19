3b326e30870fe18a7f53b1df5a1b1ab3d1f618e8
Yerden II, Gerald <Gerald.Yerden@duke-energy.com>
2018-10-18 23:06:25 -0400

    added env and deploy

--- deploy.js
+++ deploy.js
@@ -3,3 +3,29 @@
  vs.
  Our local ganache test network
  */
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+require('dotenv').config();
+
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONICPHRASE,
+    'https://rinkeby.infura.io/v3/4d0d3a2836be4ee1a5d279bf18a4a133'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account ', accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Initial Message']})
+        .send({gas: '1000000', from: accounts[0]})
+
+    console.log('Constract deployed to ', result.options.address);
+};
+
+deploy();
